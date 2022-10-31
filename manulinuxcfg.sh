b2889df3f29e271dedc87160989ebbc6c00a7320
manuelerdoes <manuel.erdoes@bluewin.ch>
2022-10-31 09:43:51 +0100

    Update manulinuxcfg.sh

--- manulinuxcfg.sh
+++ manulinuxcfg.sh
@@ -4,19 +4,17 @@ HOMEDIR=/home/manu
 SCALINGFACTOR=2
 VM=TRUE
 
-echo "adding aliases to bashrc"
 echo "alias ll='ls -lah'" >> $HOMEDIR/.bashrc
 echo "alias l='ls -lh'" >> $HOMEDIR/.bashrc
 
-echo "copying .Xresources"
 cp .Xresources $HOMEDIR/.Xresources
-echo "copying and writing .xinitrc"
 cp .xinitrc $HOMEDIR/.xinitrc
 echo "xrdb -merge $HOMEDIR/.Xresources" >> $HOMEDIR/.xinitrc
-echo "feh --bg-scale $HOMEDIR/Pictures/bg.jpg" >> $HOMEDIR/.xinitrc
+echo "feh --bg-scale $HOMEDIR/bg.jpg" >> $HOMEDIR/.xinitrc
 echo "exec dwm" >> $HOMEDIR/.xinitrc
 
-echo "install stuff"
+cp bg.jpg $HOMEDIR
+
 apt update && apt -y upgrade
 apt -y install vim rxvt-unicode git build-essential make gcc libx11-dev libxft-dev libxinerama-dev xorg xsel feh chromium dolphin suckless-tools qt5ct evince imagemagick 
 if [ "$VM" == TRUE ]
@@ -24,16 +22,13 @@ then
 	apt -y install open-vm-tools
 fi
 
-echo "install dwm"
 git clone https://git.suckless.org/dwm $HOMEDIR/dwm
 cp config.h $HOMEDIR/dwm/
 cp manudwm.sh $HOMEDIR/dwm/
 chmod +x $HOMEDIR/dwm/manudwm.sh
 (cd $HOMEDIR/dwm && $HOMEDIR/dwm/manudwm.sh)
 
-echo "do other stuff"
 apt -y purge lightdm 
 xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s $SCALINGFACTOR
 
-echo "reboot"
 systemctl reboot
